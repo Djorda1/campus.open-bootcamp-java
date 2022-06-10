c5dff0987200db9de7a3d985bda2f0a6ef3488c0
Djorda1 <106724918+Djorda1@users.noreply.github.com>
2022-06-10 18:49:02 -0300

    Add files via upload

---
+++ ModulosEj1.py
@@ -0,0 +1,11 @@
+import Operaciones.Suma as sum
+import Operaciones.resta as resta
+import Operaciones.divicion as div
+import Operaciones.multiplicacion as mul
+
+a = 4
+b = 2
+print(sum.sumar(a, b))
+print(resta.resta(a,b))
+print(div.divicion(a,b))
+print(mul.multiplicacion(a,b))
